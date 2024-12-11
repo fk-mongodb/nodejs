9f763e1a9918533f355d3a18a948a16a8d1947ea
fernando <fernando@fernando.co>
2024-12-11 08:55:17 +0800

    Add connect and list databases

---
+++ crud/connect.js
@@ -0,0 +1,46 @@
+/**
+ * @fileoverview Demonstrate how to connect to MongoDB Atlas Cluster
+ * 
+ * @description
+ * Demonstrate how to connect to MongoDB Atlas Cluster
+ * 
+ * @author Fernando Karnagi <fkarnagi@gmail.com>
+ * @version 1.0.0
+ * @date 11-Dec-2024
+ * 
+ * @usage
+ * cmd/connect.js
+ */
+
+const { MongoClient } = require('mongodb');
+
+async function main() {
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.s70hf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+    const client = new MongoClient(uri);
+
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        // Make the appropriate DB calls
+        await listDatabases(client);
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+async function listDatabases(client) {
+    const databasesList = await client.db().admin().listDatabases();
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+main().catch(console.error);
